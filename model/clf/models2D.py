dc66a30f62580036b76785b0d5b80430b467f9be
sarahgrc <sarah.garcia@epfedu.fr>
2025-03-20 14:26:31 +0100

    update

--- model/clf/models2D.py
+++ model/clf/models2D.py
@@ -7,7 +7,13 @@ import torchvision.models as models
 
 
 class ResNet(nn.Module):
-    def __init__(self, num_class:int, model_name:str, trainable_layers = None, wgth = None):
+    def __init__(self, num_class:int, model_name:str, trainable_layers = None):
+        """
+        Fine-tuned ResNet
+        :param num_class (int): Number of classes
+        :param model_name (str) : name of the resnet model to implement
+        :param trainable_layers (List): list of layers to fine tune
+        """
         super().__init__()
         available_models = {
             'ResNet50' : models.resnet50,
@@ -31,18 +37,27 @@ class ResNet(nn.Module):
             else :
                 param.requires_grad = False
 
-        if wgth :
-            self.model.load_statedict(torch.load(wgth))
-            print(f' ** Weights loadded from {wgth} **')
-
-    # TODO : add verify input channels == input channel model !!
 
     def forward(self,x):
         return self.model(x)
 
+    def load_wgts(self, wghts_path : str):
+        """
+        Load trained weights
+        :param wghts_path (str) : path of the finetuned weights
+        :return: loadded model
+        """
+        return self.model.load_state_dict(torch.load(wghts_path))
+
 
 class EfficienceNet(nn.Module):
-    def __init__(self, num_class : int, model_name:str, trainable_layers: list, wgth = None):
+    def __init__(self, num_class : int, model_name:str, trainable_layers: list):
+        """
+        EfficientNet model for fine tuning
+        :param num_class (int) : number of classes for the classification
+        :param model_name (str) : name of the efficient net to implement
+        :param trainable_layers (List(str)) :
+        """
         super().__init__()
         available_models = {
             'effb0':models.efficientnet_b0,
@@ -61,11 +76,15 @@ class EfficienceNet(nn.Module):
             if any(layer in name for layer in trainable_layers) :
                 param.requires_grad = True
             else :
-                param.requires_grad =False
-
-        if wgth:
-            self.model.load_state_dict(torch.load(wgth))
-            print(f' ** Weights loadded from {wgth} **')
+                param.requires_grad = False
 
     def forward(self, x):
         return self.model(x)
+
+    def load_wgts(self, wghts_path : str):
+        """
+        Load trained weights
+        :param wghts_path (str) : path of the finetuned weights
+        :return: loaded model
+        """
+        return self.model.load_state_dict(torch.load(wghts_path))
